acf64c76bd1fbeed2b4d5a33613bbdbcdc042d63
Keshav Yamalapati <keshav@eggheadcreative.com>
2016-09-26 00:07:12 +0530

    integrated viewpager

--- app/src/main/java/app/num/residemenu/MainActivity.java
+++ app/src/main/java/app/num/residemenu/MainActivity.java
@@ -1,13 +1,21 @@
 package app.num.residemenu;
 
 import android.content.Context;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends BaseActivity {
 
   private Context mContext;
+  ViewPager viewPager;
 
   /**
    * Called when the activity is first created.
@@ -17,7 +25,14 @@ public class MainActivity extends BaseActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     mContext = this;
+    ButterKnife.bind(this);
+    viewPager = (ViewPager) findViewById(R.id.viewPager);
     setUpMenu();
+    FragmentManager fm = getSupportFragmentManager();
+    /** Instantiating FragmentPagerAdapter */
+    MyPagerAdapter myPagerAdapter = new MyPagerAdapter(fm);
+    viewPager.setAdapter(myPagerAdapter);
+    viewPager.setCurrentItem(1);
   }
 
   /*private void changeFragment(Fragment targetFragment) {
@@ -28,5 +43,40 @@ public class MainActivity extends BaseActivity {
         .setTransitionStyle(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
         .commit();
   }*/
+  public static class MyPagerAdapter extends FragmentPagerAdapter {
+    private static int NUM_ITEMS = 3;
+
+    public MyPagerAdapter(FragmentManager fragmentManager) {
+      super(fragmentManager);
+    }
+
+    // Returns total number of pages
+    @Override
+    public int getCount() {
+      return NUM_ITEMS;
+    }
+
+    // Returns the fragment to display for that page
+    @Override
+    public Fragment getItem(int position) {
+      switch (position) {
+        case 0:
+          return new HomeFragment();
+        case 1:
+          return new ProfileFragment();
+        case 2:
+          return new CalendarFragment();
+        default:
+          return null;
+      }
+    }
+
+    // Returns the page title for the top indicator
+    @Override
+    public CharSequence getPageTitle(int position) {
+      return "Page " + position;
+    }
+
+  }
 }
 
